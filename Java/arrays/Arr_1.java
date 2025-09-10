f9a5b9a3cd64d7c71f5e11f237bf038fafafc9bd
Saina Panda <pandasaina1230@gmail.com>
2025-09-10 22:41:34 +0530

    10/09/25

---
+++ Java/arrays/Arr_1.java
@@ -0,0 +1,10 @@
+package arrays;
+import java.util.Arrays;
+
+public class Arr_1 {
+    public static void main(String[] args) {
+        int[] arr = {1, -2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
